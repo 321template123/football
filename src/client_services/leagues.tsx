3546d10892ecd009a1dca6c95f0e17da0ce50e8f
template <template@mail.ru>
2024-07-01 13:28:42 +0400

    Test fix api

--- src/client_services/leagues.tsx
+++ src/client_services/leagues.tsx
@@ -1,11 +1,11 @@
-export const loadAreas = async () => await fetch("/api/areas/")
+export const loadAreas = async () => await fetch("./api/areas/")
 
 export const loadCompetitions = async (areas?: number) => {
 	const search = new URLSearchParams()
 	console.log(areas)
 	if (areas)
 		search.append("areas", `${areas}`)
-	return await fetch(`/api/competitions/?${search.toString()}`)
+	return await fetch(`./api/competitions/?${search.toString()}`)
 }
 export const loadCompetitionCalendar = async (competiton_id: number, start_date?: string, end_date?: string) => {
 	const search = new URLSearchParams()
@@ -14,5 +14,5 @@ export const loadCompetitionCalendar = async (competiton_id: number, start_date?
 		search.append("dateFrom", start_date)
 		search.append("dateTo", end_date)
 	}
-	return await fetch(`/api/competition/?${search.toString()}`)
+	return await fetch(`./api/competition/?${search.toString()}`)
 }
